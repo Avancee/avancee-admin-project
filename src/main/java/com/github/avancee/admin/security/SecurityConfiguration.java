cd8daf98b8b3eab4f434ae78478f887cd6e480fb
byference <1170126481@qq.com>
2019-11-02 22:40:50 +0800

    add OAuth2 Server

---
+++ src/main/java/com/github/avancee/admin/security/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.github.avancee.admin.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * SecurityConfiguration
+ *
+ * @author byference
+ * @since 2019-11-02
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers().anyRequest()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/*").permitAll();
+    }
+}
